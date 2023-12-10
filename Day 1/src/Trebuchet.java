fb91ff5989c1fe8c96ebbafdba70b18eb356b44f
karnbir <karnbirr@gmail.com>
2023-12-10 00:05:48 -0800

    ugh this isn't working, try again later

--- Day 1/src/Trebuchet.java
+++ Day 1/src/Trebuchet.java
@@ -10,6 +10,7 @@ public class Trebuchet {
         //go through each line sum the values of each line
         while (inFile.hasNextLine()) {
             String line = inFile.nextLine();
+            line = StringPrep(line);
             sum += findValues(line);
         }
         System.out.println(sum);
@@ -24,7 +25,7 @@ public class Trebuchet {
         //go through string and find numbers
         for (int i = 0; i < line.length(); i++) {
 
-            if (line.charAt(i) >= '0' && line.charAt(i) <= '9') {
+            if (line.charAt(i) >= '0' && line.charAt(i) <= '9') { //remove after finished stringprep
 
                 temp = line.charAt(i) - '0';
 
@@ -35,5 +36,39 @@ public class Trebuchet {
             }
         }
         return sum + temp;
+    }
+
+    public static String StringPrep (String line) {
+
+        //this order is specific to avoid overriding numbers
+        if (line.contains("one")) {
+            line = line.replaceFirst("one","one1one");
+        }
+        if (line.contains("two")) {
+            line = line.replaceFirst("two", "two2two");
+        }
+        if (line.contains("three")) {
+            line = line.replaceFirst("three","three3three");
+        }
+        if (line.contains("four")) {
+            line = line.replaceFirst("four", "four4four");
+        }
+        if (line.contains("five")) {
+            line = line.replaceFirst("five","five5five");
+        }
+        if (line.contains("six")) {
+            line = line.replaceFirst("six", "six6six");
+        }
+        if (line.contains("seven")) {
+            line = line.replaceFirst ("seven", "seven7seven");
+        }
+        if (line.contains("eight")) {
+            line = line.replaceFirst("eight", "eight8eight");
+        }
+        if (line.contains("nine")) {
+            line = line.replaceFirst("nine","nine9nine");
+        }
+
+        return line;
     }
 }
